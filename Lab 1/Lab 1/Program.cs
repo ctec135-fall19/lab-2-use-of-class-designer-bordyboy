51512e2248852842c60d4d47cbd606ef5e8918ce
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-10-17 18:36:47 -0700

    Lab Done

---
+++ Lab 1/Lab 1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Bogdan Livadaru
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //making title beatiful
+            Console.Title = "Car Lab";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("__Welcome to my car class practice lab.___");
+
+            //initiating SportsCar object
+            SportsCar sc = new SportsCar();
+            //calling the PrintName method from the initiated object
+            sc.PrintName();
+        }
+    }
+}
---
+++ Lab 1/Lab 1/SportsCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class SportsCar : Car
+    {
+        public string GetPetName()
+        {
+            PetName = "Praf";
+            return PetName;
+        }
+
+        //prints PetName when called
+        public void PrintName()
+        {
+            Console.WriteLine(GetPetName());
+        }
+    }
+}
